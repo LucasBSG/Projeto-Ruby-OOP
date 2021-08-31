dc3f85ee33515dfeebe07833e59afa5337dc32ef
Lucas Basaglia <lcamanducci@gmail.com>
2021-08-30 23:15:43 -0300

    Novos arquivos

---
+++ variaveis/app.rb
@@ -0,0 +1,12 @@
+x = Exemplo.new("Instância 1")
+x.variavel_de_classe
+# => "Variável da classe"
+
+x.variavel_de_instancia
+# => "Instância 1"
+
+Exemplo.variavel_de_classe
+# => "Variável da classe" 
+
+Exemplo.variavel_de_instancia
+# => nil
