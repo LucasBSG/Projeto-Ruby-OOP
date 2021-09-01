4a6cd6ccbb057fecf652570f6bb5bced1360993b
Lucas Basaglia <lcamanducci@gmail.com>
2021-09-01 12:27:51 -0300

    Novos arquivos

---
+++ arrays/app.rb
@@ -0,0 +1,8 @@
+meu_array = [10, 20, 30, 40, 50, 60]
+
+meu_array[0] = "dez" # alterar 10 pela string 'dez'
+# ["dez", 20, 30, 40, 50, 60]
+
+for elemento in meu_array
+    puts elemento
+end
---
+++ hash/app.rb
@@ -0,0 +1,5 @@
+meu_hash = { :um => 1, :dois => 2, :tres => 3 }
+
+meu_hash.each do |chave, valor|
+  puts "A posição #{chave} guarda o valor #{valor}"
+end
